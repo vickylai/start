d27a4c086c2a788bf15c6cf633039e1d8b448cd4
Vicky <vicky@vickylai.io>
2017-11-11 18:42:33 -0500

    Broken CORS

--- js/starting.js
+++ js/starting.js
@@ -18,25 +18,25 @@ function getIP() {
 
 function setMood(rise, set) {
     var h = getTime();
-    if (h > set+1 || h < rise) {
+    if (h > set + 1 || h < rise) {
         // Night
         document.body.style.backgroundImage = "linear-gradient(to top, #111111 0%, #232323 50%, #454545 100%)";
     } else
-    if (h >= rise && h <= rise+1) {
-        // Sunrise
-        document.body.style.backgroundImage = "linear-gradient(to top, #fbc2eb 0%, #a6c1ee 100%)";
-        document.body.style.color = "#111111";
-    } else
-    if (h >= rise+2 && h <= set-1) {
-        // Day
-        document.body.style.backgroundImage = "linear-gradient(to top, #a1c4fd 0%, #c2e9fb 100%)";
-        document.body.style.color = "#111111";
-    } else
-    if (h == set && h <= set+1) {
-        // Sunset
-        document.body.style.backgroundImage = "linear-gradient(to bottom, #fa709a 0%, #fee140 100%)";
-        document.body.style.color = "#111111";
-    }
+        if (h >= rise && h <= rise + 1) {
+            // Sunrise
+            document.body.style.backgroundImage = "linear-gradient(to top, #fbc2eb 0%, #a6c1ee 100%)";
+            document.body.style.color = "#111111";
+        } else
+            if (h >= rise + 2 && h <= set - 1) {
+                // Day
+                document.body.style.backgroundImage = "linear-gradient(to top, #a1c4fd 0%, #c2e9fb 100%)";
+                document.body.style.color = "#111111";
+            } else
+                if (h == set && h <= set + 1) {
+                    // Sunset
+                    document.body.style.backgroundImage = "linear-gradient(to bottom, #fa709a 0%, #fee140 100%)";
+                    document.body.style.color = "#111111";
+                }
 }
 
 function getTime() {
@@ -78,82 +78,120 @@ function geoFindMe() {
             // Please use your own free key. Get it at https://darksky.net
             key = '1ab44c1eacf257905c07b689ff153685';
 
-        let apiURL = 'https://api.darksky.net/forecast/' + key + '/' + latitude + ',' + longitude;
+
+
+        // Create the XHR object.
+        function createCORSRequest(method, url) {
+            var xhr = new XMLHttpRequest();
+            if ("withCredentials" in xhr) {
+                // XHR for Chrome/Firefox/Opera/Safari.
+                xhr.open(method, url, true);
+            } else if (typeof XDomainRequest != "undefined") {
+                // XDomainRequest for IE.
+                xhr = new XDomainRequest();
+                xhr.open(method, url);
+            } else {
+                // CORS not supported.
+                xhr = null;
+            }
+            return xhr;
+        }
+
+        // Helper method to parse the title tag from the response.
+        function getTitle(text) {
+            return text.match('<title>(.*)?</title>')[1];
+        }
 
         // Get some weather info from DarkSky 
-
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', apiURL, true);
-        xhr.onload = function (e) {
-            let json = JSON.parse(this.response);
-
-            // Get, parse degree info
-            let degF = Math.round(json.currently.temperature),
-                degC = Math.round((degF - 32) * (5 / 9)),
-                degFHigh = Math.round(json.daily.data[0].temperatureMax),
-                degFLow = Math.round(json.daily.data[0].temperatureMin),
-                degCHigh = Math.round((degFHigh - 32) * (5 / 9)),
-                degCLow = Math.round((degFLow - 32) * (5 / 9)),
-                country = json.timezone,
-                unixrise = json.daily.data.sunriseTime,
-                rise = unixrise.getHours(),
-                unixset = json.daily.data.sunsetTime,
-                set = unixset.getHours();
-
-            // Place weather info
-            document.getElementById('pipeya').innerHTML = ' | ';
-
-            function loadTemp(country) {
-                if (country.indexOf('America') === 0) {
-                    document.getElementById('temp').innerHTML = degC;
-                    document.getElementById('deg').innerHTML = '°C';
-                } else {
-                    document.getElementById('temp').innerHTML = degF;
-                    document.getElementById('deg').innerHTML = '°F';
-                }
+        function makeCorsRequest() {
+            let url = 'https://api.darksky.net/forecast/' + key + '/' + latitude + ',' + longitude;
+            var xhr = createCORSRequest('GET', url);
+            if (!xhr) {
+                alert('CORS not supported');
+                return;
             }
-            loadTemp(country);
-            setMood(rise,set);
+
+            xhr.onload = function (e) {
+                let json = JSON.parse(this.response);
+
+                console.log(json);
+
+                // Get, parse degree info
+                let degF = Math.round(json.currently.temperature),
+                    degC = Math.round((degF - 32) * (5 / 9)),
+                    degFHigh = Math.round(json.daily.data[0].temperatureMax),
+                    degFLow = Math.round(json.daily.data[0].temperatureMin),
+                    degCHigh = Math.round((degFHigh - 32) * (5 / 9)),
+                    degCLow = Math.round((degFLow - 32) * (5 / 9)),
+                    country = json.timezone,
+                    unixrise = json.daily.data.sunriseTime,
+                    rise = unixrise.getHours(),
+                    unixset = json.daily.data.sunsetTime,
+                    set = unixset.getHours();
+
+                    console.log(rise, set);
+
+                // Place weather info
+                document.getElementById('pipeya').innerHTML = ' | ';
+
+                function loadTemp(country) {
+                    if (country.indexOf('America') === 0) {
+                        document.getElementById('temp').innerHTML = degC;
+                        document.getElementById('deg').innerHTML = '°C';
+                    } else {
+                        document.getElementById('temp').innerHTML = degF;
+                        document.getElementById('deg').innerHTML = '°F';
+                    }
+                }
+                loadTemp(country);
+                setMood(rise, set);
+                document.getElementById('advice').style.opacity = "0";
+                document.getElementById('page').style.opacity = "1";
+            }
+            xhr.onerror = function () {
+            };
+
+            xhr.send();
+        }
+
+        makeCorsRequest();
+    }
+
+        function error(error) {
+            output.innerHTML = "You are here.";
             document.getElementById('advice').style.opacity = "0";
             document.getElementById('page').style.opacity = "1";
         }
-        xhr.send();
-    }
 
-    function error(error) {
-        output.innerHTML = "You are here.";
-        console.log(error);
-        document.getElementById('advice').style.opacity = "0";
-        document.getElementById('page').style.opacity = "1";
-    }
+        document.getElementById('advice').style.opacity = "1";
+        document.getElementById('page').style.opacity = "0";
 
-    document.getElementById('advice').style.opacity = "1";
-    document.getElementById('page').style.opacity = "0";
+        navigator.geolocation.getCurrentPosition(success, error);
+    };
 
-    navigator.geolocation.getCurrentPosition(success, error);
-};
 
-let ready = function (fn) {
+    let ready = function (fn) {
 
-    // Sanity check
-    if (typeof fn !== 'function') return;
+        // Sanity check
+        if (typeof fn !== 'function') return;
 
-    // If document is already loaded, run method
-    if (document.readyState === 'complete') {
-        return fn();
-    }
+        // If document is already loaded, run method
+        if (document.readyState === 'complete') {
+            return fn();
+        }
 
-    // Otherwise, wait until document is loaded
-    document.addEventListener('DOMContentLoaded', fn, false);
+        // Otherwise, wait until document is loaded
+        document.addEventListener('DOMContentLoaded', fn, false);
 
-};
+    };
 
-// Example
-ready(function () {
-    document.getElementById('advice').style.opacity = "1";
-    document.getElementById('page').style.opacity = "0";
-    getTime();
-    getIP();
-    newQuote();
-    geoFindMe();
-});
+
+    // Example
+    ready(function () {
+        document.getElementById('advice').style.opacity = "1";
+        document.getElementById('page').style.opacity = "0";
+        getTime();
+        getIP();
+        newQuote();
+        geoFindMe();
+    });
